208b9ef71311db324b024135de1972fe09607a28
Marcelo Grinbaum <marcelogrinbaum@Marcelos-MacBook-Pro.local>
2017-08-14 02:04:42 -0300

    Programa final

--- src/main.c
+++ src/main.c
@@ -5,18 +5,30 @@
  * numero de palavras separadas que recebeu e, apos, encerrar.
  */
 
+//Marcelo Beinisis Grinbaum
+//RA:173577
+
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-
-  char c;
-
-  c = 1;
-
-  while (c != '\n') {
-    scanf("%c", &c);
+  
+  char texto[10000];
+  int x=0, tamanho, i=0;
+  
+  fgets(texto, 10000, stdin);
+  tamanho= strlen(texto);
+  
+  while (i<tamanho) {
+    if (texto[i] == ' ' || texto[i] == '.' || texto[i] == ',' || texto[i] == '!' || texto[i] == '\n' || texto[i] == '-' || texto[i] == ':') {
+      if ( (texto[i-1] >= 'a' && texto[i-1] <= 'z') || (texto[i-1] >= 'A' && texto[i-1] <= 'Z') || (texto[i-1] >= '0' && texto[i-1] <= '9') )
+	x++;
+      if ( (texto[i] == ',' || texto[i] == '.') && (texto[i+1] >= '0' && texto[i+1] <= '9') )
+	x--;
+    }
+    i++;
   }
-
-  printf("1\n");
+  
+  printf("%d\n", x);
   return 0;
 }
